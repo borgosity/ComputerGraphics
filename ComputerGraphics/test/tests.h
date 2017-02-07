8e57df806c24bca57cdf213eff195833972770da
borgosity <borgosity@gmail.com>
2017-02-07 22:11:17 +1100

    add Gizmo and gl_core_4_4

--- ComputerGraphics/test/tests.h
+++ ComputerGraphics/test/tests.h
@@ -1,16 +1,10 @@
 #pragma once
-#include <glew.h>
-#include <glfw3.h>
-#include <glm.hpp>
+#include <glew/glew.h>
+#include <glfw/glfw3.h>
+#include <glm/glm.hpp>
 
 // source includes
-#include "DisplayManager.h"
+
 
 // tests
 void helloWorld();
-void helloTriangle();
-void renderEngineTest();
-
-// miscellaneous functions
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-void screenColour(GLfloat *red, GLfloat *green, GLfloat *blue, GLboolean *allColour);
