c6a93b817b913c82b678031e16cc0b23f5ffdd56
borgosity <borgosity@gmail.com>
2017-02-08 12:26:56 +1100

    minor

--- ComputerGraphics/test/tests.cpp
+++ ComputerGraphics/test/tests.cpp
@@ -43,7 +43,8 @@ bool helloWorld()
 		// Render
 		// Clear the colorbuffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
